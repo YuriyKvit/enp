38b5ce059b85a7899c0cfc93feff2b5de61a5e81
yuriy.kvit <asmoteh@gmail.com>
2018-01-22 14:19:36 +0200

    some refactor, change web3 lib

--- eth.js
+++ eth.js
@@ -1,10 +1,4 @@
-var Web3 = require('web3');
+var Web3EthAccounts = require('web3-eth-accounts');
 
-if (typeof web3 !== 'undefined') {
-    web3 = new Web3(web3.currentProvider);
-} else {
-    // set the provider you want from Web3.providers
-    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-}
-// web3.providers.HttpProvider('http://localhost:8545');
-console.log(web3);
+var account = new Web3EthAccounts('ws://localhost:8546');
+account.create();
