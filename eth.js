af0a3d70e7aa0fa28acd08b51a6cbb31614e4980
yuriy.kvit <asmoteh@gmail.com>
2018-01-23 11:52:45 +0200

    Add db to ignore

--- eth.js
+++ eth.js
@@ -1,5 +1,5 @@
 var Web3EthAccounts = require('web3-eth-accounts');
 
-var account = new Web3EthAccounts('ws://localhost:8546');
+var account = new Web3EthAccounts();
 var cred = account.create();
 console.log(cred);
