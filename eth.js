5ed88d3120e621d1517df023eef52683a08a9d2f
yuriy.kvit <asmoteh@gmail.com>
2018-01-24 16:47:36 +0200

    Temp commit

--- eth.js
+++ eth.js
@@ -1,6 +1,16 @@
-var Accounts = require('web3-eth-accounts');
-
-var accounts = new Accounts();
-
-var cred = accounts.create();
-console.log(cred);
+let Web3 = require('web3');
+let net = require('net');
+let web3 = new Web3(new Web3.providers.IpcProvider('\\\\.\\pipe\\geth.ipc', net));
+if (true === web3.isConnected()) {
+    console.log('Connected to IPC.');
+}
+// wraps web3 sync/callback function into a Promise
+function web3AsynWrapper(web3Fun) {
+    return function (arg) {
+        return new Promise((resolve, reject) => {
+            web3Fun(arg, (e, data) => e ? reject(e) : resolve(data))
+        })
+    }
+}
+let account = web3AsynWrapper(web3.personal.newAccount);
+account("qwerty123").then(console.log);
--- main.js
+++ main.js
@@ -1,34 +1,42 @@
 let Web3 = require('web3');
 let net = require('net');
+// let personal = require('web3-eth-personal');
 
 let web3 = new Web3(new Web3.providers.IpcProvider('\\\\.\\pipe\\geth.ipc', net));
 if (true === web3.isConnected()) {
-    console.log('Connected.');
+    console.log('Connected to IPC.');
 }
-
-
+let eth = {};
+let tx = {
+    from: '0x16C73B1D2422879E11D7BC33Da6cC5E34099374B',
+    to: '0x16C73B1D2422879E11D7BC33Da6cC5E34099374B',
+    value: '1000000000000000'
+};
+eth.balance = web3AsynWrapper(web3.eth.getBalance);
+eth.send = web3AsynWrapper(web3.eth.sendTransaction);
+eth.unlock = web3AsynWrapper(web3.personal.unlockAccount);
+// eth.accounts = web3AsynWrapper(web3.eth.accounts);
 // wraps web3 sync/callback function into a Promise
-function web3AsynWrapper (web3Fun) {
+function web3AsynWrapper(web3Fun) {
     return function (arg) {
         return new Promise((resolve, reject) => {
             web3Fun(arg, (e, data) => e ? reject(e) : resolve(data))
-    })
+        })
     }
 }
-let ballance = web3AsynWrapper(web3.eth.getBalance);
-ballance("0x16C73B1D2422879E11D7BC33Da6cC5E34099374B")
-    .then(console.log);
+// console.log(eth.accounts);
+eth.unlock('1f25e98b60a3528704264d2754f410a1733e29d4', "qwerty123");
+// eth.send(tx, '0xeabafee1572c828fe8249e8f107c2ab9bbcd40c8fd49de47cbf008298c5cec4f').then(console.log);
 
 
-// let send = web3AsynWrapper(web3.eth.sendTransaction);
 // // using the event emitter
 // send({
 //     from: '0x1fb99a783ef5b6502d74e997856b7fa48843ac29',
 //     to: '0x1fb99a783ef5b6502d74e997856b7fa48843ac29',
 //     value: '1000000000000000'
-// },function(error, hash){
-// console.log('Error5: '+error);
-// console.log('Hash: '+hash);
+// }, function (error, hash) {
+//     console.log('Error5: ' + error);
+//     console.log('Hash: ' + hash);
 // });
 // var Accounts = require('web3-eth-accounts');
 
